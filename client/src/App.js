75003e97c8e392b0c69b6c8c6c66690db057f785
polin <chypailenko@gmail.com>
2019-01-06 10:35:21 +0200

    added list of items

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import AppNavbar from './copmonents/AppNavbar';
+import ShoppingList from './copmonents/ShoppingList';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
@@ -9,6 +10,7 @@ class App extends Component {
     return (
       <div className="App">
         <AppNavbar/>
+        <ShoppingList/>
       </div>
     );
   }
